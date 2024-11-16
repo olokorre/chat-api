582ac873443e2b906cdc83b07e33fc31b3e1c95c
Andrei Ruan Steffens <andreiruansteffens@gmail.com>
2024-11-16 15:55:50 -0300

    Implementado tipo de mensagem

--- lib/domain/entity/message.dart
+++ lib/domain/entity/message.dart
@@ -1,7 +1,13 @@
+import 'package:chat_dart/domain/type/message_type.dart';
+
 class Message {
   final int id;
   final String content;
   final DateTime dateTime;
   final int userId;
-  const Message(this.id, this.content, this.dateTime, this.userId);
+  late final MessageType type;
+  Message(this.id, this.content, this.dateTime, this.userId,
+      {MessageType? type}) {
+    this.type = type ?? MessageType.text;
+  }
 }
--- test/message_test.dart
+++ test/message_test.dart
@@ -1,4 +1,5 @@
 import 'package:chat_dart/domain/entity/message.dart';
+import 'package:chat_dart/domain/type/message_type.dart';
 import 'package:test/test.dart';
 
 void main() {
@@ -22,5 +23,17 @@ void main() {
       1,
     );
     expect(message.userId, 1);
+  });
+
+  test("Deve constatar que a mensagem é do tipo texto", () {
+    final dateTime = DateTime.parse('2024-04-01 10:00:00');
+    final message = Message(1, 'content', dateTime, 1);
+    expect(message.type, MessageType.text);
+  });
+
+  test("Deve criar uma mensagem do timpo imagem", () {
+    final dateTime = DateTime.parse('2024-04-01 10:00:00');
+    final message = Message(1, 'content', dateTime, 1, type: MessageType.image);
+    expect(message.type, MessageType.image);
   });
 }
